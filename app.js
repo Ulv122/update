90bb062873ee5338c15e35deb4ac93f4451908d3
Ulv122 <qemmmberov22@gmail.com>
2025-04-20 10:25:19 +0400

    update

---
+++ app.js
@@ -0,0 +1,4 @@
+let data = '{"id":356,"type":"general","joke":"Why does a chicken coop only have two doors? Because if it had four doors it would be a chicken sedan."}';
+let  a = JSON.parse(data);
+a.id = 157;
+console.log(obj);
